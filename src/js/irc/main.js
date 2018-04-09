f0ddc4f0240d7b787c31281da4815a4bcc6db3e0
krynen <krynen2530@gmail.com>
2018-04-09 09:05:06 +0900

    irc(2)
    onmessage 이벤트를 바인드해 response와 제대로 연결

--- src/js/irc/main.js
+++ src/js/irc/main.js
@@ -6,8 +6,6 @@ module.exports = function(uniformData) {
     this.ws = {};
     
     var CRLF = String.fromCharCode(13) + String.fromCharCode(10);
-    var response = this.response;
-    var ws = this.ws;
     
     this.connect = function() {
       /* 데이터 로드 */
@@ -27,25 +25,25 @@ module.exports = function(uniformData) {
       if (channel==null || channel.match(/^[A-Za-z0-9_]+$/)==null) { throw ["ircWrongChannel", "Error"]; }
       
       /* 웹소켓을 열고 초기 발신 메세지를 설정 */
-      try { ws = new WebSocket(data.uri); }
+      try { this.ws = new WebSocket(data.uri); }
       catch(event) { throw ["ircConnectFail", "Error", event.message]; }
-      ws.onopen = function() {
-        ws.send("PASS " + data.pass + CRLF);
-        ws.send("NICK " + data.nick + CRLF);
-        ws.send("CAP REQ :" + data.capabilities.join(" ") + CRLF);
-        ws.send("JOIN #" + channel + CRLF);
+      this.ws.onopen = function() {
+        this.send("PASS " + data.pass + CRLF);
+        this.send("NICK " + data.nick + CRLF);
+        this.send("CAP REQ :" + data.capabilities.join(" ") + CRLF);
+        this.send("JOIN #" + channel + CRLF);
       };
       
       /* 메세지 수신부 설정 */
-      ws.onmessage = function(event) {
+      this.ws.onmessage = (function(event) {
         var lines = event.data.toString().split(/\r\n|\r|\n/);
         if (lines[lines.length-1].length == 0) { lines.pop(); }
-        lines.forEach( function(line) { response(line); } );
-      };
+        lines.forEach( function(line) { this.response(line); }, this);
+      }).bind(this);
       
       /* 브라우저가 켜져 있는 동안 항시 접속을 유지하므로,
          비정상적으로 연결이 해제되었을 경우엔 예외처리 */
-      ws.onclose = function(event) {
+      this.ws.onclose = function(event) {
         throw ["ircClosed", "Error", "error code "+event.code];
       };
     };
@@ -54,8 +52,8 @@ module.exports = function(uniformData) {
       var arguments = line.split(/\s/);
       
       /* PING-PONG을 통한 접속 유지 */
-      if (arguments[0].toLowerCase() == "ping") {
-        ws.send("PONG" + CRLF);
+      if (arguments[0] == "PING") {
+        this.ws.send("PONG" + CRLF);
       }
       
       /*
