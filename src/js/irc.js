14e9c47e44e861f4153304d38336c20482f55d92
krynen <krynen2530@gmail.com>
2018-08-27 08:49:33 +0900

    IRC 접속
    ***
    *** IRC 접속
    ***
    : 트위치 IRC에 접속하여 채팅을 읽어온다
    
    - src/js/irc.js
    : 접속 및 명령어 전송을 구현 (22-26, 33-57)
    : 모듈 인터페이스를 마저 구현 (8-15, 66-70)
    
    - src/json/shared.json
    : IRC 접속에 필요한 정보를 추가 (2-10)
    
    - src/json/default.json
    : 잘못된 설정값, 접속 오류시 표시할 오류 메세지를 설정 (14, 15)

--- src/js/irc.js
+++ src/js/irc.js
@@ -5,19 +5,69 @@
 
 // 모듈 인터페이스
 var methods = {};
-var data = null;
+var data = {
+  socket : null   // IRC에 접속할 웹소켓. 생성자를 사용하므로 그 전까진 참조할 수 없다
+};
+
+// 포인터 정의
+var config  = null;
+var shared  = null;
+var message = null;
 
 
+/**
+ * 웹소켓에 메세지 전송
+ * @param {...string} text
+ */
+var send = function(text) {
+  for (var i=0; i<arguments.length; ++i) {
+    data.socket.send(arguments[i] + "\r\n");
+  };
+}
+
+/**
+ * 트위치 IRC에 접속
+ * 이후 onmessage 이벤트를 통해 통신
+ */
 methods.Connect = function() {
+  // 채널이름이 제대로 되었는지 파싱
+  var channel = config.Channel;
+  if (typeof channel !=="string" || channel.match(/\W/) !== null) {
+    message.Error("Irc_Wrong_Channel");
+  }
+
+  // 트위치 IRC서버로 접속을 시도
+  try { this.socket = data.socket = new WebSocket(shared.Irc.Uri); }
+  catch(err) { message.Error("Irc_Fail_Connect", [err.message] ); return; }
+
+  // 접속 성공시 목표 채널 참가를 시도하고 Compability를 설정
+  data.socket.onopen = function() {
+    // pass는 의미는 없지만 난수로. nick은 justinfan+난수로.
+    // justinfan으로 접속시 게스트모드로 참여할 수 있다
+    var rand = function(n) { return Math.floor(Math.random()*(`1${"0".repeat(n)}`)) };
+    var pass = rand(8);
+    var nick = "justinfan" + rand(5);
+
+    send(
+      `PASS ${pass}`,
+      `NICK ${nick}`,
+      `CAP REQ :${shared.Irc.Caps.join(" ")}`,
+      `JOIN #${channel}`
+    );
+  };
 };
 
 
 /**
- * 테스트용 로드 메서드
- * main.js에서 연결된 이후에 호출됨
+ * 모듈 Load 메서드
+ * 포인터를 메인 모듈과 연결
  * @param {object} uniformData 메인 모듈 오브젝트
  */
-methods.Load = function(uniformData) {};
+methods.Load = function(uniformData) {
+  config = uniformData.Data.config;
+  shared = uniformData.Data.shared;
+  message = uniformData.Message;
+};
 
 
 /**
