c508a5e9f983f474812887c8550800dfc1c66db1
krynen <krynen2530@gmail.com>
2018-09-03 12:11:58 +0900

    IRC 통신
    ***
    *** IRC 통신
    ***
    - src/js/irc.js
    : IRC 메세지에 대응 (59-63, 76-203)
    : 연결 종료시 오류메세지 출력 (65-69)
    : 연결시 shared.Id를 채널 아이디로 설정 (67, 135-140, 153-158)
    
    - src/json/shared.json
    : 설정할 채널 아이디 데이터를 추가 (2)
    
    - src/json/default.json
    : IRC 메세지와 연관된 오류메세지 추가 (16, 18, 20, 21)
    
    - config.js
    : 추가된 설정을 예시 파일에도 적용 (24-40)

--- config.js
+++ config.js
@@ -10,12 +10,34 @@ var value =
  * 이하 블록을 수정하여 설정을 변경       *
  ******************************************/
 {
+  // 접속할 채널 아이디
   "Channel" : "ninja",
+
   "Data"    : {
+    // 추가로 불러올 (웹상의) json 설정 파일
     "Uris"  : {
       "type"  : "replace",
       "value" : []
     }
+  },
+
+  "Error" : {
+    // 오류 메세지 상세 정보 출력 여부
+    "Detailed" : true,
+
+    // 각 오류 메세지 문자열
+    "Message_No_NormalRoot" : "TemplateNormalRoot Template을 찾지 못했습니다.",
+    "Message_No_ErrorRoot"  : "TemplateErrorRoot와 TemplateNormalRoot Template을 찾지 못했습니다.",
+
+    "Data_Fail_Config" : "설정 파일을 불러오는데 실패했습니다.",
+    "Data_Fail_Theme"  : "테마 템플릿을 불러오는데 실패했습니다.",
+
+    "Irc_Notice"          : "트위치 채팅 서버에 문제가 발생한 것 같습니다.",
+    "Irc_Wrong_Channel"   : "잘못된 트위치 유저 아이디입니다.",
+    "Irc_Success_Connect" : "채팅 서버에 무사히 접속되었습니다.",
+    "Irc_Fail_Connect"    : "채팅 서버와의 접속에 실패했습니다.",
+    "Irc_Close_Connect"   : "채팅 서버와의 연결이 종료되었습니다.",
+    "Irc_Wrong_Message"   : "처리되지 않는 메세지입니다."
   }
 }
 ;
--- src/js/irc.js
+++ src/js/irc.js
@@ -55,6 +55,152 @@ methods.Connect = function() {
       `JOIN #${channel}`
     );
   };
+
+  data.socket.onmessage = function(event) {
+    event.data.toString().split(/[\r\n]+/).forEach( function(el) {
+      if (!el.match(/^[\s]*$/)) { methods.Response(el); }
+    } );
+  };
+
+  data.socket.onclose = function(event) {
+    // 연결중이지 않을 때의 아이디는 null로 고정
+    shared.Id = null;
+    message.Error("Irc_Close_Connect", event.code);
+  };
+};
+
+/**
+ * 받은 IRC 메세지에 대응
+ * @param {string} line 줄별로 나뉘어진 메세지
+ */
+methods.Response = function(line) {
+  // 공백으로 각 구문을 구분
+  var arguments = line.split(/\s/);
+
+  // 핑에 자동응답하여 접속을 유지
+  if (arguments[0] === "PING") {
+    send("PONG");
+    return;
+  }
+
+  // PING외의 모든 메세지는 :또는 @으로 시작하므로 구분
+  var prefix = arguments[0][0];
+
+  // IRC 기본 프로토콜 메세지 처리
+  if (prefix === ":") {
+    switch (arguments[1]) {
+      // 접속시 고정 표시 메세지 무시
+      case "001": case "002": case "003": case "004":
+      case "375": case "372": case "376": case "366": case "CAP":
+      
+      // 유저 참가 및 퇴장 메세지 무시
+      case "JOIN": case "PART":
+      case "MODE": case "353" :
+
+      // 타 채널로 호스팅 메세지 무시
+      case "HOSTTARGET":
+        return;
+
+      // 트위치 내부 문제가 발생했을 때
+      case "NOTICE":
+        message.Error("Irc_Notice", arguments.splice(3, arguments.length).join(" ").substring(1));
+        return;
+
+      default:
+        message.Error("Irc_Wrong_Message", prefix+arguments[1]);
+        return;
+    }
+  }
+
+  // Twitch-specific 메세지 처리
+  if (prefix === "@") {
+    // 메세지 데이터들을 파싱
+    var subArguments = arguments.shift().substring(1).split(";").reduce( function(acc, cur) {
+      var keyValue = cur.split("=");
+      if (keyValue[1] !== undefined) {
+        switch(keyValue[0]) {
+          // 필요 없는 데이터 생략
+          case "id":          // 메세지 아이디
+          case "turbo":       // 트위치 터보 이용 여부
+          case "tmi-sent-ts": // 메세지 타임스탬프
+          case "user-type":   // 유저 타입("mod", "")
+            break;
+
+          // 뱃지 데이터 정리
+          case "badges":
+            acc[keyValue[0]] = keyValue[1].split(",");
+            break;
+
+          // 채널 아이디 (첫 접속 메세지에 필요)
+          case "room-id":
+            if (shared.Id === null) {
+              acc[keyValue[0]] = keyValue[1];
+            } else {
+              break;
+            }
+
+          // 특별히 처리할 필요 없는 데이터 반영
+          default:
+            acc[keyValue[0]] = keyValue[1];
+            break;
+        }
+      }
+
+      return acc;
+    }, {});
+
+    switch (arguments[1]) {
+      case "ROOMSTATE":   // 채널 접속 혹은 방의 상태 변경
+        if (subArguments["room-id"] !== undefined) {
+          shared.Id = subArguments["room-id"];
+          message.Error("Irc_Success_Connect");
+        }
+        return;
+
+      case "PRIVMSG":     // 일반 메세지
+        return;
+
+      case "CLEARCHAT":   // 채팅 전체삭제 혹은 유저 차단
+        if (subArguments["ban-reason"] === undefined) {
+
+        } else {
+
+        }
+        return;
+
+      case "USERNOTICE":  // 유저 관련 메세지
+        switch(subArguments["msg-id"]) {
+          case "sub":
+          case "resub":       // 신규 구독 및 재구독
+            return;
+
+          case "subgift":     // 구독 선물
+            return;
+
+          case "raid":        // 다른 채널에서 온 레이드
+            return;
+
+          case "ritual":      // 채널에 처음 온 시청자
+            return;
+
+          case "rewardgift":  // 응원으로 인한 선물 나눔
+            return;
+
+          default:
+            message.Error("Irc_Wrong_Message", prefix+arguments[1]+" "+subArguments["msg-id"]);
+            return;
+        }
+
+      case "NOTICE":      // 방 상태 변경
+        return;
+
+      default:
+        message.Error("Irc_Wrong_Message", prefix+arguments[1]);
+        return;
+    }
+  }
+
+  message.Error("Irc_Wrong_Message", line);
 };
 
 
