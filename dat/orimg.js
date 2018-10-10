afb75111000631f9d58f91738665970c884155b5
krynen <krynen2530@gmail.com>
2018-10-11 03:58:34 +0900

    파일 정리
    ***
    *** 파일 정리
    ***
    - src/html/client.html
    : 브라우저에서 실행될 때를 위해 타이틀과 파비콘 추가 (8-9)
    : 잘못 입력된 탭 문자를 공백으로 치환 (4)
    
    - dat/orimgs.js
    : 예시 전용이미지 파일을 명료하게 표기 (11-38)
    
    - src/json/default.html
    : 파싱을 방해하는 오브젝트의 마지막 요소 다음 반점을 제거 (82)
    
    - .gitignore
    : 주석 추가 (1, 5, 8)
    : 디렉터리를 표시 (2, 3, 6, 9)

--- dat/orimg.js
+++ dat/orimg.js
@@ -3,53 +3,38 @@
  * 브라우저의 sessionStorage에 설정을 추가 *
  * 모듈에서 불러올 수 있도록 함            *
  *                                         *
+ * 유틸리티를 통해 수정할 것               *
+ *                                         *
  *******************************************/
-
-var orimg = 
-/******************************************
- * 이하 블록을 수정하여 설정을 변경       *
- ******************************************/
-{
-  "Prefix"  : "~",
-  "UriBase" : "dat/orimgs/",
-  "Groups"  : [
-    { "name":"목록",   "method":"none",     "rule":"이미지\\d" },
-    { "name":"랜덤",   "method":"random",   "rule":"\\d+" },
-    { "name":"순서",   "method":"sequence", "list":["이미지3", "이미지2", "이미지1", "이미지0"] },
-    { "name":"아이디", "method":"fixed",    "rule":"\\d+" }
-  ],
-  "Images"  : [
-    { "name":"0", "uri":"0.png", "alias":["이미지0"] },
-    { "name":"1", "uri":"1.png", "alias":["이미지1"] },
-    { "name":"2", "uri":"2.png", "alias":["이미지2"] },
-    { "name":"3", "uri":"3.png", "alias":["이미지3"] },
-    { "name":"4", "uri":"4.png" },
-    { "name":"5", "uri":"5.png" },
-    { "name":"6", "uri":"6.png" },
-    { "name":"7", "uri":"7.png" },
-    { "name":"8", "uri":"8.png" },
-    { "name":"9", "uri":"9.png" }
-  ]
-}
-;
-
-/**
- * 포맷 변환부
- * 설정 포맷으로 orimg를 변환
- */
-var value = (function() {
-  orimg.Groups = { "type":"replace", "value":orimg.Groups };
-  orimg.Images = { "type":"replace", "value":orimg.Images };
-  return { "Message":{"Orimg":orimg} };
-})();
-
-(
-/**
- * 세션 등록부
- * sessionStorage에 value object를 추가
- */
-function() {
+(function() { 
   var key = "data/config/" + (document.scripts.length - 1);
+  var value = { "Message" : { "Orimg" : {
+    "Prefix"  : "~",
+    "UriBase" : "dat/orimgs/",
+    "Groups"  : {
+      "type"   : "replace",
+      "value"  : [
+        { "name":"목록",   "method":"none",     "rule":"이미지\\d" },
+        { "name":"랜덤",   "method":"random",   "rule":"\\d+" },
+        { "name":"순서",   "method":"sequence", "list":["이미지3", "이미지2", "이미지1", "이미지0"] },
+        { "name":"아이디", "method":"fixed",    "rule":"\\d+" }
+      ]
+    },
+    "Images"  : {
+      "type"   : "replace",
+      "value"  : [
+        { "name":"0", "uri":"0.png", "alias":["이미지0"] },
+        { "name":"1", "uri":"1.png", "alias":["이미지1"] },
+        { "name":"2", "uri":"2.png", "alias":["이미지2"] },
+        { "name":"3", "uri":"3.png", "alias":["이미지3"] },
+        { "name":"4", "uri":"4.png" },
+        { "name":"5", "uri":"5.png" },
+        { "name":"6", "uri":"6.png" },
+        { "name":"7", "uri":"7.png" },
+        { "name":"8", "uri":"8.png" },
+        { "name":"9", "uri":"9.png" }
+      ]
+    }
+  } } };
   sessionStorage.setItem(key, JSON.stringify(value));
-}
-)();
+})();
